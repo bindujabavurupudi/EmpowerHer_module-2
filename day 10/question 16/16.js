304a1fa99b7d5c13ee977f0fc92051f62246de64
bindujabavurupudi <bindujabavurupudi2006@gmail.com>
2025-11-24 23:37:30 +0530

    added questions

--- day 10/question 16/16.js
+++ day 10/question 16/16.js
@@ -1,7 +1,9 @@
 const isEven = n => n % 2 === 0
 
-
-result = marks >= 35 ? "Pass" : "Fail";
+let marks = 45
+const result = marks >= 35 ? "Pass" : "Fail";
+console.log(result)
 
 
 const greet = (name) => `Hello, ${name ? name : "Guest"}`;
+console.log(greet("Munni"))
