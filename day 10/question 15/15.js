1d988761be1b5b111764125f51847c92ab6e5f85
bindujabavurupudi <bindujabavurupudi2006@gmail.com>
2025-11-24 23:17:01 +0530

    added questions 15

---
+++ day 10/question 15/15.js
@@ -0,0 +1,20 @@
+let username = "binduja";
+let course = "Js";
+console.log(`Hello ${username}, welcome to the ${course} course!`)
+
+
+
+const name1 = "Sam";
+const age = 21;
+const student = {
+  name1,
+  age,
+  greet: function () {
+    console.log("Hello");
+  }
+};
+
+
+
+
+const getFullName = (first, last) => `${first} ${last}`;
