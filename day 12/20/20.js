0465be784ef7331e55cd346fb5b92e6b4c2e5a4e
bindujabavurupudi <bindujabavurupudi2006@gmail.com>
2025-11-26 23:31:50 +0530

    added q20

--- day 12/20/20.js
+++ day 12/20/20.js
@@ -1,11 +1,48 @@
-let age = 20;
-const displayAge = () => {
-    console.log(age);
-}
-displayAge();
 
-const changeAge = () =>{
-    age = age+1;
-    console.log(age);
+// Question 1
+function createCounter(){
+    let count = 0;
+    return {
+        increment : () =>{
+            count++;
+            console.log(`Current count: ${count}`)
+        },
+        decrement : () =>{
+            count--;
+            console.log(`Current count: ${count}`)
+        }
+    }
 }
-changeAge();
+
+const counter = createCounter();
+counter.increment(); 
+counter.increment(); 
+counter.decrement(); 
+
+
+
+
+//  Question 2
+function createBankAccount(){
+    let amt = 0;
+    return {
+        deposit : (amount) =>{
+            amt += amount;
+            console.log(`Deposited: ${amount}`)
+        },
+        withdraw :  (amount) =>{
+            if(amt>=amount){
+                amt -= amount;
+                console.log(`Withdrawn: ${amount}`)
+            }
+            else 
+                console.log("Insufficient balance")
+        }
+    }
+}
+const account = createBankAccount();
+account.deposit(500); // Output: Deposited: 500
+account.withdraw(200); // Output: Withdrawn: 200
+account.withdraw(400); // Output: Insufficient balance
+console.log(account.balance); // Output: undefined
+
