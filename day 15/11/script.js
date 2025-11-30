26f60d8331f0fb21bb64497c17c8ae9440de3cd4
bindujabavurupudi <bindujabavurupudi2006@gmail.com>
2025-11-30 22:07:53 +0530

    added q11

---
+++ day 15/11/script.js
@@ -0,0 +1,6 @@
+let h1 = document.getElementById("welcome")
+h1.textText= "Welcome to the DOM World!";
+let p = document.getElementsByTagName("p")
+for(let i = 0; i < p.length; i++)
+        p[i].style.color = "blue";
+document.querySelector(".container").style.backgroundColor = "yellow";
